14bc3bb07f0e3245e60adf1dd0e9ffa425938297
Jonah Middleton <jonah.middleton@gmail.com>
2021-01-21 19:08:38 -0500

    Replace custom calendar with date-fns
    calendar code was complicated and unreliable using external library fixed issues and is much more robust for future use

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,78 +1,81 @@
-import Calendar from "./calendar.js";
+import * as dateFns from 'https://esm.run/date-fns';
 
-document.addEventListener('DOMContentLoaded', (event) => {
-	addCounters();
+document.addEventListener('DOMContentLoaded', event =>{
+    addCounters();
 });
 
+const anniversaryDate = {
+    year: 2017,
+    month: 8,
+    day: 23
+}
+
 const addCounters = () => {
+    const upCounter = document.getElementById('counter-from');
+    const downCounter = document.getElementById('counter-until');
 
-	countFrom(new Date("August 23, 2017 00:00:00"));
+    countUp(upCounter);
+    countDown(downCounter);
 
-	countTill()
+    setInterval(() => {
+        countUp(upCounter);
+        countDown(downCounter);
+    }, 1000 * 60);
 }
 
-const countFrom = (startDate) => {
-	let now = new Date();
-	
-	const counter = document.getElementById('counter-from');
-
-	updateCounter(now, startDate, counter);
-	
-	
-	setInterval(() => {
-		now = new Date()
-		updateCounter(now, startDate, counter);
-
-	}, 1000 * 60);
-
+const countUp = counter => {
+    const anniversary = new Date(anniversaryDate.year, anniversaryDate.month, anniversaryDate.day);
+    const now = new Date();
+    
+    const time = dateDifference(now, anniversary);
+    updateCounter(time, counter);
 }
 
-const countTill = () => {
-	//TODO: Day of anniversary
-	let now = new Date();
-	let nextDate = findNextAnniversary();
-	const counter = document.getElementById('counter-until');
+const countDown = counter => {
+    const next = findNextAnniversary();
+    const now = new Date();
 
-	if(nextDate === null){
-		counter.innerHTML = "Happy Anniversary!"
-	}else{
-		updateCounter(now, nextDate, counter);
-	}
-	
-	setInterval(() => {
-		now = new Date();
-		nextDate = findNextAnniversary();
-
-		if(nextDate === null){
-			counter.innerHTML = "Happy Anniversary!"
-		}else{
-			updateCounter(now, nextDate, counter);
-		}
-	}, 1000 * 60);
+    const time = dateDifference(next, now);
+    updateCounter(time, counter);
 }
 
-const updateCounter = (startDate, endDate, counter) => {
-	const time = Calendar.getDifference(startDate, endDate);
+const updateCounter = (time, counter) => {
+    let countStr = ""
+    time.forEach(part => {
+        console.log(part.unit + ": " + part.value);
+        let str = part.value + " " + part.unit + ((part.value != 1)? "s " : " ");
+        countStr += str;
+    });
 
-	counter.innerHTML = time.years + " year(s), " + time.months + " month(s), " + time.days + " day(s), and " + time.hours + " hour(s)";
+	counter.innerHTML = countStr;
+}
+
+const dateDifference = (endDate, startDate) => {
+    const result = [];
+    const parts = ['year', 'month', 'day', 'hour'];
+
+    parts.forEach((part, i) => {
+        let p = part.charAt(0).toUpperCase() + part.slice(1);
+        let t = dateFns[`differenceIn${p}s`](endDate, startDate);
+        if(t){
+            result.push({unit: part, value: t});
+            if(i < parts.length){
+                endDate = dateFns[`sub${p}s`](endDate, t);
+            }
+        }
+    });
+    return result;
 }
 
 const findNextAnniversary = () => {
-	const dateNow = new Date();
-	let next;
-
-	if(dateNow.getMonth() < 8){
-		next = new Date("September 23, " + dateNow.getFullYear() + " 00:00:00");
-	}else if(dateNow.getMonth() == 8 && dateNow.getDate() <= 23){
-		if(dateNow.getDate() != 23){
-			next = new Date("September 23, " + dateNow.getFullYear() + " 00:00:00");
-		}else{
-			next = null;
-		}
-	}else{
-		next = new Date("September 23, " + (dateNow.getFullYear() + 1) + " 00:00:00");
-	}
-
-	return next;
-	
-} 
+    let next = new Date(new Date().getFullYear(), anniversaryDate.month, anniversaryDate.day);
+    const diff = dateFns.differenceInDays(next, new Date());
+    
+    if(diff > 0){
+        return next;
+    }else if(diff < 0){
+        return dateFns.addYears(next, 1);
+    }else{
+        return null;
+    }
+}
